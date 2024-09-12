31b2db9dd385de4007d138fbeaa12cf80042eae5
AdanaTatuyaHatsumi <kasirunsitorus.51@gmail.com>
2024-09-13 03:31:11 +0800

    simpan perubahan

---
+++ undangan.php
@@ -0,0 +1,328 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="css/bootstrap.min.css" />
+    <link rel="stylesheet" href="css/font-awesome.min.css" />
+    <link rel="stylesheet" href="css/owl.carousel.min.css" />
+    <link rel="stylesheet" href="css/owl.theme.default.min.css" />
+
+    <!-- AOS -->
+    <link rel="stylesheet" href="css/aos.css" />
+
+    <!-- Google Fonts -->
+    <link rel="stylesheet" href="css/google-fonts.css" />
+
+    <link rel="icon" href="image/icon-cincin.png" />
+
+    <!-- Fontawesome -->
+    <link rel="stylesheet" href="css/all.css" />
+    <link rel="stylesheet" href="css/fontawesome.min.css" />
+
+    <!-- Simply Countdown -->
+    <link rel="stylesheet" href="countdown/simplyCountdown.theme.default.css" />
+    <script src="countdown/simplyCountdown.js"></script>
+
+    <!-- Main CSS -->
+    <link rel="stylesheet" href="css/main.css" />
+
+    <title>Raka & Sania</title>
+  </head>
+  <body>
+    <!-- musik -->
+    <audio id="audio" autoplay>
+      <source
+        src="musik/Beautiful and White - Westlife.mp3"
+        type="audio/mpeg"
+      />
+    </audio>
+    <button onclick="playPause()" id="play" title="" class="bg-transparent">
+      <i class="fa-solid fa-music"></i>
+    </button>
+    <!-- End of musik -->
+
+    <!-- Hero -->
+    <section class="hero">
+      <div class="container">
+        <div class="row">
+          <div class="col text-center" data-aos="fade-up">
+            <p>Kepada Bapak/Ibu/Saudara/I</p>
+            <h1>Raka & Sania</h1>
+            <p>Akan melangsungkan pernikahan dalam:</p>
+            <div class="simply-countdown pb-3"></div>
+            <a href="#acara" class="btn btn-cta p-3">Save the date</a>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- End of Hero -->
+
+    <!-- Couple Section -->
+    <section class="couple">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12 col-md-12 text-center" data-aos="fade-down">
+            <img src="image/ornament3.png" class="img-ornament" alt="" />
+            <h3>Acara Pernikahan</h3>
+            <h4>Diselenggarakan pada 11 November 2023 di South Korea</h4>
+            <p>
+              Oleh karena itu, dengan segala hormat kami bermaksud untuk
+              mengundang Bapak/Ibu, Saudara/i, untuk acara pernikahan kami.
+            </p>
+          </div>
+        </div>
+
+        <div class="row py-3">
+          <div class="col-lg-6">
+            <div class="row">
+              <div class="col-8 text-end py-4" data-aos="fade-right">
+                <h3 class="nama-mempelai">Raka Narendra</h3>
+                <p class="nama-ortu">
+                  Putra pertama dari pasangan <br />
+                  Bapak Solihun dan Ibu Siti
+                </p>
+              </div>
+
+              <div class="col-4" data-aos="fade-right">
+                <img
+                  src="image/foto-pria.jpg"
+                  class="img-responsive rounded-circle"
+                  alt=""
+                />
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-6" data-aos="fade-left">
+            <div class="row">
+              <div class="col-4">
+                <img
+                  src="image/foto-wanita.jpg"
+                  class="img-responsive rounded-circle"
+                  alt=""
+                />
+              </div>
+              <div class="col-8 text-start py-4">
+                <h3 class="nama-mempelai">Sania Alkana</h3>
+                <p class="nama-ortu">
+                  Putri kedua dari pasangan <br />
+                  Bapak Rahmat dan Ibu Zainab
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- End of Couple-->
+
+    <!-- Acara -->
+    <section id="acara" class="acara">
+      <div class="container">
+        <div class="row" data-aos="fade-right">
+          <div class="col-lg-9 col-md-12 mx-auto text-center">
+            <img src="image/ornament3.png" class="img-ornament" alt="" />
+            <h3>Assalamualaikum Warahmatullahi <br/>Wabarakatuhu</h3>
+            <p>
+              Dengan memohon Ridho serta Rahmat Allah SWT, kami bermaksud
+              menyelenggarakan pernikahan putra-putri kami yang Insya Allah akan
+              diselenggarakan pada :
+            </p>
+            <img
+              src="image/ornamen-text.png"
+              class="img-fluid img-responsive"
+              alt=""
+            />
+          </div>
+        </div>
+
+        <div class="row my-3" data-aos="fade-up">
+          <div class="col text-center">
+            <a href="" class="btn btn-cta"
+              ><i class="fa-solid fa-location-dot"></i> Peta Lokasi</a
+            >
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- End of Acara -->
+
+    <!-- True Love -->
+    <section class="hadist">
+      <div class="container">
+        <div class="row" data-aos="fade-up-left">
+          <div class="col-lg-9 mx-auto col-md-12 text-center">
+            <img src="image/ornament3.png" class="img-ornament pb-3" alt="" />
+            <h3>The True Love</h3>
+            <img src="image/true-love img.png" class="pb-3" alt="" />
+            <p>
+              “Dan di antara tanda-tanda kekuasaan-Nya ialah Dia menciptakan
+              untukmu isteri-isteri dari jenismu sendiri, supaya kamu cenderung
+              dan merasa tenteram kepadanya, dan dijadikan-Nya diantaramu rasa
+              kasih dan sayang. Sesungguhnya pada yang demikian itu benar-benar
+              terdapat tanda-tanda bagi kaum yang berfikir.” - Q.S. Ar - Rum:21
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- End of True Love -->
+
+    <!-- Ucapan -->
+    <section class="ucapan">
+      <div class="container">
+        <div class="row">
+          <div class="col mx-auto text-center">
+            <img
+              src="image/ornament3.png"
+              class="img-ornament"
+              alt=""
+              data-aos="fade-in"
+            />
+            <h3 data-aos="fade-in">Ucapan & Doa</h3>
+            <p data-aos="fade-in">Berikan ucapan kepada kedua mempelai</p>
+            <form>
+              <div class="mb-3">
+                <input
+                  type="name"
+                  class="form-control mx-auto mb-3"
+                  id="nama"
+                  aria-describedby="emailHelp"
+                  placeholder="Nama"
+                />
+                <input
+                  type="name"
+                  class="form-control mx-auto ucapan-doa"
+                  id="ucapan"
+                  aria-describedby="emailHelp"
+                  placeholder="Ucapan & Doa"
+                />
+
+                <a href="index.html" class="btn btn-cta mt-3">Kirim</a>
+              </div>
+            </form>
+            <hr />
+            <div class="container wrapper py-4">
+              <div class="komentar-ucapan">
+                <h6 class="username fw-bold">Fulano Rahardi</h6>
+                <p>Selamat atas pernikahannyaa..</p>
+              </div>
+              <hr />
+              <div class="komentar-ucapan">
+                <h6 class="username fw-bold">Fulano Rahardi</h6>
+                <p>Selamat atas pernikahannyaa..</p>
+              </div>
+              <hr />
+              <div class="komentar-ucapan">
+                <h6 class="username fw-bold">Fulano Rahardi</h6>
+                <p>Selamat atas pernikahannyaa..</p>
+              </div>
+              <hr />
+              <div class="komentar-ucapan">
+                <h6 class="username fw-bold">Fulano Rahardi</h6>
+                <p>Selamat atas pernikahannyaa..</p>
+              </div>
+              <hr />
+              <div class="komentar-ucapan">
+                <h6 class="username fw-bold">Fulano Rahardi</h6>
+                <p>Selamat atas pernikahannyaa..</p>
+              </div>
+              <hr />
+              <div class="komentar-ucapan">
+                <h6 class="username fw-bold">Fulano Rahardi</h6>
+                <p>Selamat atas pernikahannyaa..</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- End of Ucapan -->
+
+    <!-- GIFTS -->
+    <section class="gifts">
+      <div class="container">
+        <div class="row">
+          <div class="col mx-auto text-center" data-aos="fade-down">
+            <img src="image/ornament-gifts.png" alt="" />
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- End of GIFTS -->
+
+    <!-- Terima Kasih -->
+    <section class="thanks">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-8 mx-auto text-center" data-aos="fade-up">
+            <img src="image/ornament3.png" class="img-ornament" alt="" />
+            <h3>Terima Kasih</h3>
+            <p>
+              Merupakan suatu Kehormatan dan Kebahagiaan Bagi Kami, Apabila
+              Bapak/Ibu/Saudara/i Berkenan Hadir Untuk memberikan Doa Restu
+              Kepada kedua mempelai.
+            </p>
+            <p class="py-3">Kami yang berbahagia</p>
+          </div>
+
+          <div class="row" data-aos="fade-down">
+            <div class="col-lg-12">
+              <div class="nama-calon-pengantin">
+                <h3>Raka</h3>
+                <img src="image/icon-cincin.png" alt="" />
+                <h3>Sania</h3>
+              </div>
+              <div class="text-center">
+                <img
+                  src="image/ornament-thanks.png"
+                  class="img-thanks text-center mx-auto"
+                  alt=""
+                />
+              </div>
+            </div>
+          </div>
+
+          <div class="row footer text-center mx-auto">
+            <div class="col">
+              <img src="image/logo-adana-digital.png" alt="" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- End of Terima Kasih -->
+  </body>
+
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/jquery.min.js"></script>
+  <script src="js/owl.carousel.min.js"></script>
+  <script src="js/custom.js"></script>
+  <script src="js/audio.js"></script>
+  <script src="js/aos.js"></script>
+
+  <script>
+    simplyCountdown(".simply-countdown", {
+      year: 2023, // required
+      month: 12, // required
+      day: 1, // required
+      hours: 0, // Default is 0 [0-23] integer
+      words: {
+        //words displayed into the countdown
+        days: { singular: "hari", plural: "hari" },
+        hours: { singular: "jam", plural: "jam" },
+        minutes: { singular: "menit", plural: "menit" },
+        seconds: { singular: "detik", plural: "detik" },
+      },
+      refresh: 1000, //default refresh every 1s
+      sectionClass: "simply-section", //section css class
+      amountClass: "simply-amount", // amount css class
+      wordClass: "simply-word", // word css class
+      zeroPad: false,
+      countUp: false, // enable count up if set to true
+    });
+  </script>
+</html>
